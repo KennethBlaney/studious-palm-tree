9323cbfedc2581a2f46f33b74164d3b1e9c91cb5
Kenneth Blaney <kennethrblaney@gmail.com>
2025-03-08 01:15:46 -0500

    file set up, first functions

---
+++ src/utils/roll.py
@@ -0,0 +1,27 @@
+from random import randint
+from math import fabs
+
+
+def roll_d100(advantage: int = 0) -> int:
+    """
+    Makes a percentile dice roll
+    :param advantage: a measure of advantage, positive or negative, taking the best or worst of multiple rolls
+    :return: a value from the dice roll
+    """
+    rolls = []
+    for _ in range(0, int(fabs(advantage))+1):
+        rolls.append(randint(1, 100))
+    if advantage > 0:
+        return min(rolls)
+    return max(rolls)
+
+
+def roll_ndm(n: int = 0, m: int = 0) -> int:
+    """
+    Makes a roll of n copies of dice with m sides each
+    :return: the sum from the dice roll
+    """
+    s = 0
+    for _ in range(0, n):
+        s += randint(1, m)
+    return s
