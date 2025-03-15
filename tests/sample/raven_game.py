d1272519bd9177e6c24e89cffde803949f40cfe5
Kenneth Blaney <kennethrblaney@gmail.com>
2025-03-14 20:02:24 -0400

    add 'guilt' mechanics to sample game

--- tests/sample/raven_game.py
+++ tests/sample/raven_game.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Dict
+
 from dataclasses_json import dataclass_json, Undefined
 
 from src.sheets import BasicRoleplaySkill, BasicRoleplayCharacter
@@ -9,8 +11,43 @@ from src.sheets import BasicRoleplaySkill, BasicRoleplayCharacter
 class RavenSkill(BasicRoleplaySkill):
     guilt: int = 100
 
+    def guilt_roll(self,
+                   category_bonus: dict = None,
+                   armor_penalty: dict = None,
+                   fatigue_points: int = 0,
+                   diff_multi: int = 1,
+                   modifier: int = 0,
+                   advantage: int = 0,
+                   lucky: bool = False,
+                   guilt: int = 0) -> Dict:
+        if guilt >= self.guilt:
+            return {
+                "fumble": False,
+                "failure": True,
+                "success": False,
+                "special": False,
+                "critical": False,
+                "guilt": True
+            }
+        result = self.skill_roll(category_bonus,
+                                 armor_penalty,
+                                 fatigue_points,
+                                 diff_multi,
+                                 modifier,
+                                 advantage,
+                                 lucky)
+        result.update({"guilt": False})
+
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class RavenCharacter(BasicRoleplayCharacter):
     guilt: int = 10
+
+    def add_guilt(self, amount: int = 0):
+        if amount < 0:
+            return
+        self.guilt += amount
+
+    def reset_guilt(self):
+        self.guilt = 0
